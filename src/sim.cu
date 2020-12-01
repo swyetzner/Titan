29433f95126cb8cf8c658a988f85cf6cc48026a1
Andrew Moshova <andrew@moonbark.com>
2020-12-01 08:56:20 -0500

    Moved fourier run inside the step loop

--- src/sim.cu
+++ src/sim.cu
@@ -1733,31 +1733,34 @@ void Simulation::step(double size) {
     while(stepped < size) {
 
         //for (int i = 0; i < NUM_QUEUED_KERNELS; i++) {
-            //cudaDeviceSynchronize();
+        //cudaDeviceSynchronize();
 
-            computeSpringForces<<<springBlocksPerGrid, THREADS_PER_BLOCK>>>(d_spring, springs.size(), T); // compute mass forces after syncing
-            gpuErrchk( cudaPeekAtLastError() );
+        computeSpringForces << < springBlocksPerGrid, THREADS_PER_BLOCK >> >
+                                                      (d_spring, springs.size(), T); // compute mass forces after syncing
+        gpuErrchk(cudaPeekAtLastError());
 
-            massForcesAndUpdate<<<massBlocksPerGrid, THREADS_PER_BLOCK>>>(d_mass, global, d_constraints, masses.size());
-            gpuErrchk( cudaPeekAtLastError() );
+        massForcesAndUpdate << < massBlocksPerGrid, THREADS_PER_BLOCK >> >
+                                                    (d_mass, global, d_constraints, masses.size());
+        gpuErrchk(cudaPeekAtLastError());
 
-            T += dt;
+        T += dt;
 
         //}
 
         stepped += dt;
-    }
 
-    if (fourier) {
-        //printf("%d %d, last record %f, n_count %d, n %d\n", T - fourier->last_recorded > fourier->ts,
-        //       fourier->n_count < fourier->n, fourier->last_recorded, fourier->n_count, fourier->n);
-        if (fourier->n_count < fourier->n) {
-            // RUN SDFT KERNEL
-            discreteFourierTransform<<<massBlocksPerGrid, THREADS_PER_BLOCK>>>(d_mass, d_fourier_pointers->d_fourier, masses.size());
-            fourierFromArray(); // Get kernel data
-            fourier->n_count++;
-            fourier->last_recorded = T;
-            fourierToArray(); // Set updated variables on GPU
+        if (fourier) {
+            //printf("%d %d, last record %f, n_count %d, n %d\n", T - fourier->last_recorded > fourier->ts,
+            //       fourier->n_count < fourier->n, fourier->last_recorded, fourier->n_count, fourier->n);
+            if (fourier->n_count < fourier->n) {
+                // RUN SDFT KERNEL
+                discreteFourierTransform << < massBlocksPerGrid, THREADS_PER_BLOCK >> >
+                                                                 (d_mass, d_fourier_pointers->d_fourier, masses.size());
+                fourierFromArray(); // Get kernel data
+                fourier->n_count++;
+                fourier->last_recorded = T;
+                fourierToArray(); // Set updated variables on GPU
+            }
         }
     }
 
