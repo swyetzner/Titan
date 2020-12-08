e9a84701c84d8b5230c8aec8bca04d2fdfc4a7c5
scavicchio <salvy@nyu.edu>
2020-12-08 15:49:27 -0500

    t

--- src/sim.cu
+++ src/sim.cu
@@ -1258,7 +1258,8 @@ __global__ void computeSpringForces(CUDA_SPRING ** d_spring, int num_springs, do
             default: break;
         }
 
-        double tempNorm = temp.norm()+(1E-6*(tempNorm==0));
+        double tempNorm = temp.norm();
+        tempNorm = tempNorm+(1E-6*(tempNorm==0));
       //  if (tempNorm == 0) {
       //      tempNorm = 1E-6;
       //  }
